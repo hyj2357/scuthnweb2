a74195398df4c4f2215ec927f1e2ea643f353988
Mac <Mac@Mac-PC>
2016-02-29 09:51:56 +0800

    m

--- scuthnweb2/src/com/scuthnweb/listener/LoginSessionListener.java
+++ scuthnweb2/src/com/scuthnweb/listener/LoginSessionListener.java
@@ -1,53 +1,63 @@
 package com.scuthnweb.listener;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 import com.scuthnweb.dao.AccountDao;
 import com.scuthnweb.dao.Login_sessionDao;
 import com.scuthnweb.domain.Login_session;
 
 public class LoginSessionListener implements HttpSessionListener{
-	private Login_sessionDao login_sessionDao;
-	private AccountDao accountDao;
 	
 	@Override
 	public void sessionCreated(HttpSessionEvent event) {
-	
+		System.out.println(event.getSession().getId()+" session create!");
 	}
 
 	@Override
 	public void sessionDestroyed(HttpSessionEvent event) {
+		//System.out.println("Have Listened session "+event.getSession().getId()+" destroyed\n");
 		HttpSession session = event.getSession();
 		Integer uid = (Integer)session.getAttribute("user_id");
 		if(uid!=null){
-			List ls = this.login_sessionDao.findByUid(uid);
-			if(ls.size()==0)
-				return;
-			else{
-				this.login_sessionDao.delete((Login_session)ls.get(0));
-			}			
+			this.deleteLogin_session(uid);
+		    session.removeAttribute("user_id");
+			session.removeAttribute("user_account");			
 		}
-		else
+		else{
+			System.out.println("Session NO Login!");
 			return;
+		}
 	}
-
-	public Login_sessionDao getLogin_sessionDao() {
-		return login_sessionDao;
-	}
-
-	public void setLogin_sessionDao(Login_sessionDao login_sessionDao) {
-		this.login_sessionDao = login_sessionDao;
-	}
-
-	public AccountDao getAccountDao() {
-		return accountDao;
-	}
-
-	public void setAccountDao(AccountDao accountDao) {
-		this.accountDao = accountDao;
+	
+	private void deleteLogin_session(Integer uid){
+		String driver = "com.mysql.jdbc.Driver";  
+        String url = "jdbc:mysql://127.0.0.1:3306/scuthnwebdatabase";  
+        String user = "root";  
+        String pwd = "123456";  
+        try{  
+            Class.forName(driver);  
+            Connection connect = (Connection) DriverManager.getConnection(url,user,pwd);  
+            if(!connect.isClosed()){  
+                System.out.println("succeed in getting the sql connection");  
+                //执行sql语句对象  
+                Statement statement = (Statement) connect.createStatement();  
+                //Sql语句  
+                String sql = "DELETE FROM log_session l WHERE l.uid="+uid+";";  
+                ResultSet rs = statement.executeQuery(sql);  
+                System.out.println("--------------------------");  
+                System.out.println("------执行结果如下-------");  
+                System.out.println("--------------------------");  
+                rs.close();  
+                connect.close();  
+            }  
+        }catch(Exception e){}    
 	}
 }
