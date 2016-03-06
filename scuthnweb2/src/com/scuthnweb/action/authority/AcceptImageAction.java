e47630595a3b6e7ca1a614848cabf7d966259f07
hyj2357 <976350901@qq.com>
2016-03-06 21:14:03 +0800

    m

--- scuthnweb2/src/com/scuthnweb/action/authority/AcceptImageAction.java
+++ scuthnweb2/src/com/scuthnweb/action/authority/AcceptImageAction.java
@@ -27,7 +27,9 @@ public class AcceptImageAction extends ActionSupport{
 	public String execute(){
 		System.out.println("*************************** Accept Image **************************************");
 		System.out.println(this.uid+":"+this.sid+" "+this.path);
-		if(this.queryValidateModule.isSessionLogin(uid, sid)){
+		boolean is = this.queryValidateModule.isSessionLogin(uid, sid);
+		System.out.println(is);
+		if(is){
 			System.out.println(type+" "+uid+" "+path+"\n");
 			if(type.equals(USER_ICON))
 				this.userAdModule.uploadUserIcon(uid, path);
